eb1d30cba9f5cab6cea2d98dcb8fa058d68c2677
Alexander Polakov <plhk@sdf.org>
2012-07-17 15:51:28 +0400

    Add 'Enter' key

--- vi.c
+++ vi.c
@@ -170,6 +170,12 @@ vi_mode(GtkWidget *widget, GdkEventKey *event) {
 			obj = ParaEnd;
 			m = -1;
 			break;
+		case GDK_Return:
+		case GDK_ISO_Enter:
+			move(widget, Line, 1, 0);
+			obj = ParaEnd;
+			m = -1;
+			break;
 		case GDK_A:
 			mode = Insert;
 			/* FALLTHROUGH */
